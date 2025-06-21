6a53fbd73a078903bd55839c2f92e18fb7fec6ac
tuskyp <bravl2010@bk.ru>
2025-06-21 20:44:47 +0300

    add more static files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use actix_files::Files;
-use actix_web::{App, HttpServer};
+use actix_web::{App, Error, HttpResponse, HttpServer, get};
+use std::fs;
+
+#[get("/")]
+async fn home() -> Result<HttpResponse, Error> {
+    let html = fs::read_to_string("static/index.html").map_err(Error::from)?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(html))
+}
+#[get("/reg")]
+async fn register() -> Result<HttpResponse, Error> {
+    let html = fs::read_to_string("static/register.html").map_err(Error::from)?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(html))
+}
+#[get("/login")]
+async fn login() -> Result<HttpResponse, Error> {
+    let html = fs::read_to_string("static/login.html").map_err(Error::from)?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(html))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            // Раздаём файлы из папки `static` по корневому URL `/`
-            .service(Files::new("/", "./static").index_file("index.html"))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(home).service(register).service(login))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
